99e2bec77763a40a41ba13bdb8d9aa9c97a724ae
Konstantin Anikeev <anikeew@gmail.com>
2022-09-18 02:27:40 +0300

    force create account command

--- src/Xioru.Messaging/ServiceExtensions.cs
+++ src/Xioru.Messaging/ServiceExtensions.cs
@@ -10,76 +10,76 @@ using Xioru.Messaging.Contracts.Messenger;
 using Xioru.Messaging.Messenger;
 using Xioru.Messaging.MessengerCommand;
 
-namespace Xioru.Messaging
+namespace Xioru.Messaging;
+
+public static class ServiceExtensions
 {
-    public static class ServiceExtensions
+    public static IServiceCollection AddMessagingServices(
+        this IServiceCollection services,
+        IConfiguration config)
     {
-        public static IServiceCollection AddMessagingServices(
-            this IServiceCollection services,
-            IConfiguration config)
+        services.AddTransient<IMessengerRepository, MongoMessengerRepository>();
+
+        // register user messenger commands
+        services.AddTransient<IMessengerCommand, UnameCommand>();
+        services.AddTransient<IMessengerCommand, PwdCommand>();
+        services.AddTransient<IMessengerCommand, CdCommand>();
+        services.AddTransient<IMessengerCommand, InviteCommand>();
+        services.AddTransient<IMessengerCommand, JoinCommand>();
+        services.AddTransient<IMessengerCommand, StartCommand>();
+        services.AddTransient<IMessengerCommand, MecCommand>();
+        services.AddTransient<IMessengerCommand, LeaveCommand>();
+        services.AddTransient<IMessengerCommand, HelpCommand>();
+
+        // supervisor commands
+        services.AddTransient<IMessengerCommand, SinviteCommand>();
+        services.AddTransient<IMessengerCommand, SpwdCommand>();
+        services.AddTransient<IMessengerCommand, ScdCommand>();
+
+        //common channel commands
+        services.AddTransient<IChannelCommand, ListCommand>();
+        services.AddTransient<IChannelCommand, DetailsCommand>();
+        services.AddTransient<IChannelCommand, SaccountCommand>();
+
+        // validators
+        services.AddValidatorsFromAssemblyContaining<ChannelGrain>();
+
+        // mappers
+        services.AddAutoMapper(typeof(ChannelGrain));
+
+        // register messengers
+        var botsSection = config.GetSection(BotsSection.SectionName);
+        if (botsSection.Exists())
         {
-            services.AddTransient<IMessengerRepository, MongoMessengerRepository>();
+            var messengersConfigs = botsSection.Get<BotsSection>()?.Configs;
 
-            // register user messenger commands
-            services.AddTransient<IMessengerCommand, UnameCommand>();
-            services.AddTransient<IMessengerCommand, PwdCommand>();
-            services.AddTransient<IMessengerCommand, CdCommand>();
-            services.AddTransient<IMessengerCommand, InviteCommand>();
-            services.AddTransient<IMessengerCommand, JoinCommand>();
-            services.AddTransient<IMessengerCommand, StartCommand>();
-            services.AddTransient<IMessengerCommand, MecCommand>();
-            services.AddTransient<IMessengerCommand, LeaveCommand>();
-            services.AddTransient<IMessengerCommand, HelpCommand>();
-
-            // supervisor commands
-            services.AddTransient<IMessengerCommand, SinviteCommand>();
-            services.AddTransient<IMessengerCommand, SpwdCommand>();
-            services.AddTransient<IMessengerCommand, ScdCommand>();
-
-            //common channel commands
-            services.AddTransient<IChannelCommand, ListCommand>();
-            services.AddTransient<IChannelCommand, DetailsCommand>();
-
-            // validators
-            services.AddValidatorsFromAssemblyContaining<ChannelGrain>();
-
-            // mappers
-            services.AddAutoMapper(typeof(ChannelGrain));
-
-            // register messengers
-            var botsSection = config.GetSection(BotsSection.SectionName);
-            if (botsSection.Exists())
+            if (messengersConfigs != null)
             {
-                var messengersConfigs = botsSection.Get<BotsSection>()?.Configs;
+                services.AddOptions<BotsSection>()
+                    .Bind(botsSection);
 
-                if (messengersConfigs != null)
-                {
-                    services.AddOptions<BotsSection>()
-                        .Bind(botsSection);
+                services.RegisterMessengerIfEnabled<IDiscordMessengerGrain>(
+                    messengersConfigs, MessengerType.Discord);
 
-                    services.RegisterMessengerIfEnabled<IDiscordMessengerGrain>(
-                        messengersConfigs, MessengerType.Discord);
-
-                    services.RegisterMessengerIfEnabled<ITelegramMessengerGrain>(
-                        messengersConfigs, MessengerType.Telegram);
-                }
+                services.RegisterMessengerIfEnabled<ITelegramMessengerGrain>(
+                    messengersConfigs, MessengerType.Telegram);
             }
-
-            return services;
         }
 
-        private static void RegisterMessengerIfEnabled<T>(
-            this IServiceCollection services,
-            Dictionary<MessengerType, MessengerSection> configs,
-            MessengerType messengerType) where T : IMessengerGrain
+        return services;
+    }
+
+    private static void RegisterMessengerIfEnabled<T>(
+        this IServiceCollection services,
+        Dictionary<MessengerType, MessengerSection> configs,
+        MessengerType messengerType) where T : IMessengerGrain
+    {
+        if (configs.TryGetValue(messengerType, out var messenger)
+            && messenger != null
+            && messenger.Enable)
         {
-            if (configs.TryGetValue(messengerType, out var messenger)
-                && messenger != null
-                && messenger.Enable)
-            {
-                services.AddTransient<IMessengerGrain>(
-                    sb => sb.GetService<IGrainFactory>()!.GetGrain<T>(Guid.Empty));
-            }
+            services.AddTransient<IMessengerGrain>(
+                sb => sb.GetService<IGrainFactory>()!.GetGrain<T>(Guid.Empty));
         }
     }
 }
